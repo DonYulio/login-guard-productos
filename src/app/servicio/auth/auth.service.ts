147bd11f0abd0fa46064fdf78c313b0229ff1f52
Julio Herrera <ju.herrerau@profesor.duoc.cl>
2024-10-28 16:18:07 -0300

    Página simple de login

--- src/app/servicio/auth/auth.service.ts
+++ src/app/servicio/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { CuerpoLogin } from './../../interfaces/CuerpoLogin';
 import { UsuarioLogeado } from './../../interfaces/UsuarioLogeado';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +11,9 @@ export class AuthService {
   private readonly URL_LOGIN: string = 'https://dummyjson.com/auth/login';
   public usuarioLogeado: UsuarioLogeado | null = null;
   public accessToken: string | null = null;
+  // Observador de cargando
+  private $cargando = new BehaviorSubject<boolean>(false);
+  public cargando = this.$cargando.asObservable();
   constructor(
     private http: HttpClient
   ) {
@@ -17,6 +21,8 @@ export class AuthService {
   }
 
   public iniciarSesion(nombre_usuario: string, contrasenia: string){
+    this.$cargando.next(true);
+
     const cuerpo: CuerpoLogin = {
       username: nombre_usuario,
       password: contrasenia
@@ -29,6 +35,7 @@ export class AuthService {
     .subscribe(resultado  => {
       this.usuarioLogeado = resultado;
       this.accessToken = resultado.accessToken;
+      this.$cargando.next(false);
       console.log(resultado);
     });
 
